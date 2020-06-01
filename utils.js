79f757ba19f9d861441e03c1e3fd1f960b35b069
Waqas Hassan <waqashsn@yahoo.com>
2020-06-01 23:19:31 +0500

    Added functionality to detect and not allow notes with duplicate titles

--- utils.js
+++ utils.js
@@ -2,25 +2,37 @@ const fs = require("fs");
 const chalk = require("chalk");
 // const yargs = require('yargs');
 
+// function to add new note; takes in title and both of the note
 const addNote = function (title, body) {
+    
     // get exiting notes
     const notes = getNotes();
 
-    const note = {
-        date_time: new Date(),
-        title: title,
-        body: body,
-    };
-    // append new note to existing notes
-    notes.push(note);
-    // stringify notes object
-    const notes_json = JSON.stringify(notes);
-    // write to notes.json file
-    fs.writeFileSync('./notes.json', notes_json);
-    console.log(chalk.green("New note added!"));
-
+    // first see if a note with same title exits; if not, add the new note
+    same_titles = notes.filter(function(note){
+        return title === note.title
+    })
+    // if there are no notes with same title, add note
+    if(same_titles.length === 0){
+        
+        const note = {
+            date_time: new Date(), // timestamp for the note
+            title: title,
+            body: body,
+        };
+        // append new note to existing notes
+        notes.push(note);
+        // stringify notes object
+        const notes_json = JSON.stringify(notes);
+        // write to notes.json file
+        fs.writeFileSync('./notes.json', notes_json);
+        console.log(chalk.green.inverse.bold("DONE!"), chalk.green("New note added!"));
+    } else {
+        console.log(chalk.white.inverse.bold("ERROR!"), chalk.red("Another note with same title exists. Please use a different title."))
+    }
 };
 
+// function to get all notes in notes.json file (database)
 const getNotes = function () {
     try {
         // this part runs if there is an existing note.json file
